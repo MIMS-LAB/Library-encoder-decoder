1c78c9f26dc98f55f3d236198d67a2b94bad225f
Ahmad Ziad Ziad Zain Aldeen <11441348+LazieKat@users.noreply.github.com>
2021-06-14 00:26:47 -0400

    new automated tests
    this test will call encoder automatically with random values

--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,82 @@
-import decoder_rrc_v2_2 as d
+####    imports    ####
+
+import os, sys, random
+import time as t
+sys.path.insert(1, "C:/Users/ahmad/OneDrive/Documents/GitHub/RRC/Library-RRC-encoder/src")
+import rrc_decoder as d
 
 
-port = "COM2"
+####    test setup    ####
+
+TX_PORT = "COM10"           # encoder transmitting port
+RX_PORT = "COM11"           # decoder recieving port
+BAUD    = "57600"           # baudrate
+TEST    = d.RRC_HEAD_GPS_LONG   # data header to test
+
+
+####    encoder setup    ####
+
+PATH    = "C:/Users/ahmad/OneDrive/Documents/GitHub/RRC/Library-RRC-encoder/tests"
+command = PATH+"/test"  
+test    = lambda header, data, time : os.system(f"{command} {TX_PORT} {header} {data} {time}")  # encoder call lambda
+
+os.system(f"gcc {PATH}/test.c -o {command} -std=c99")  # compile encoder using gcc
+
+
+####    initilization    ####
 
 while True:
-	try:
-		d.setPort(port, "57600")
-		break
-	except Exception as e:
-		print(e)
-		port = str(input("Enter a new port number: "))
-		port = "COM" + port.strip()
+    port = RX_PORT
+    baud = BAUD
+
+    try:
+        radio = d.radioConnection(port, baud)
+        break
+    except Exception as e:
+        print(e)
+        exit(-1)
 
 print("Connected")
 
-while True:
-	packet = d.getPackets()
-	if packet == None:
-		continue
 
-	print('[{}]'.format(', '.join(hex(x) for x in packet)))
+####    set ranges    ####
 
-	result = d.decodePackets(packet)
+if TEST in [d.RRC_HEAD_GPS_LAT, d.RRC_HEAD_GPS_LONG]:
+    lowerRange = -1_800_000
+    upperRange = 1_800_000
+    multiplier = 10_000
+else:
+    lowerRange = -4_000
+    upperRange = 4_000
+    multiplier = 100
 
-	print("The decoded data is: " + str(result[2]))
-	print("The checksums is: " + hex(result[1]))
-	print("The header is: " + hex(result[0]))
-	print("The time stamp is: " + str(result[3]))
-	print("Data corruption: " + str(result[4]))
+
+####    test for 1000 random values    ####
+
+for i in range(1000):
+    data = random.randint(lowerRange, upperRange) / multiplier
+    time = random.randint(0, 0xfffff)
+
+    test(TEST, data, time)
+
+    packets = radio.getPackets()
+
+    if packets == None:
+        print("an error happend")
+        continue
+
+    result = d.decodePackets(packets)
+
+    if result["corrupted"]:
+        print("CORRUPT: " + str(i))
+        continue
+
+    result.pop("checksum")
+
+    if { "header" : TEST, "data" : data, "time" : time, "corrupted" : False } == result:
+        print("OK: " + str(i))
+    else:
+        print("ERROR: " + str(i))
+    
+    # print(packets)
+    # print(result)
