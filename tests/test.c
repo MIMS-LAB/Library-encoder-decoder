8cbebf76b5e3cac085490ec5f0c403c2b1752221
Ahmad Ziad Ziad Zain Aldeen <11441348+LazieKat@users.noreply.github.com>
2021-06-14 00:28:24 -0400

    new encoder test interface
    taking data as arguments to allow automation

--- tests/test.c
+++ tests/test.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include <windows.h>
-#include "encoder_rrc_v2_2.h"
 
-HANDLE serialHandle;
+#include "../src/rrc_encoder.h"
 
-/**
- * @brief Ask the user for a new loop
- * @param None
- * @return true for a new loop or false to exit
- */
-int newLoop()
-{
-	return 1;
-    char input;
-    fflush(stdin);
-    printf("Another loop? ");
-    scanf("%c", &input);
-    fflush(stdin);
-    return input == 'y';
-}
+
+
+
+// /**
+//  * @brief Ask the user for a new loop
+//  * @param None
+//  * @return true for a new loop or false to exit
+//  */
+// int newLoop()
+// {
+// 	return 1;
+//     char input;
+//     fflush(stdin);
+//     printf("Another loop? ");
+//     scanf("%c", &input);
+//     fflush(stdin);
+//     return input == 'y';
+// }
 
 
 /**
@@ -27,62 +31,84 @@ int newLoop()
  * @param None
  * @return a HANDLE type for the COM port
  */
-HANDLE setComPort()
+HANDLE setComPort(char *portName)
 {
-	serialHandle = CreateFile("\\\\.\\COM13", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    HANDLE serialHandle = CreateFile(portName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
-	if(serialHandle == INVALID_HANDLE_VALUE) {
-		printf("handle creation failed\n");
-		if(GetLastError() == ERROR_FILE_NOT_FOUND)
-			printf("file not found\n");
-	}
+    if(serialHandle == INVALID_HANDLE_VALUE) {
+        printf("handle creation failed");
+        if(GetLastError() == ERROR_FILE_NOT_FOUND)
+            printf(": file not found");
 
-	DCB serialPropertise = {0};
-	serialPropertise.DCBlength = sizeof(serialPropertise);
+        printf("\n");
 
-	if(!GetCommState(serialHandle, &serialPropertise))
-		printf("error getting state\n");
+        exit(-1);
+    }
 
-	serialPropertise.BaudRate = CBR_57600;
-	serialPropertise.ByteSize = 8;
-	serialPropertise.fParity = 0;
-	serialPropertise.Parity = NOPARITY;
-	if(!SetCommState(serialHandle, &serialPropertise))
-		printf("error setting state\n");
+    DCB serialPropertise = {0};
+    serialPropertise.DCBlength = sizeof(serialPropertise);
 
-	return serialHandle;
+    if(!GetCommState(serialHandle, &serialPropertise))
+        printf("error getting state\n");
+
+    serialPropertise.BaudRate = CBR_57600;
+    serialPropertise.ByteSize = 8;
+    serialPropertise.fParity = 0;
+    serialPropertise.Parity = NOPARITY;
+    if(!SetCommState(serialHandle, &serialPropertise))
+        printf("error setting state\n");
+
+    return serialHandle;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	setComPort();
+    unsigned int  header, time;
+    char     comPort[] = "\\\\.\\";
+    double   data;
+    uint8_t  result[RRC_DATAPACK_SIZE];
+    DWORD    bytesWritten;
+    HANDLE   serialHandle;
 
-	do 
-	{
-		float num;
-		int time, header;
-		uint32_t int_data;
-		uint8_t test[8];
-		DWORD bytesWritten;
+    if(argc < 5)
+    {
+        printf("Too few arguments\n");
+        exit(-1);
+    }
 
-		printf("enter a number, header, and a time stamp: ");
-		scanf("%f %d %d", &num, &header, &time);
+    strcat(comPort, argv[1]);
+    header = atoi(argv[2]);
+    data   = atof(argv[3]);
+    time   = atoi(argv[4]);
 
-		encode(num, header, time, test);
+    serialHandle = setComPort(comPort);
 
-		printf("The encoded number is: %.4f\n", num);
-		printf("The encoded data is: ");
-		for(int i = 0; i < 8; i++) printf("%x ", test[i]);
-		printf("\n");
-		int_data = _float2int(num, header);
-		printf("The integer sent is: %d\n", int_data);
-		printf("The checksums is: %x\n", _checksum(int_data));
-		printf("The header is: %x\n", header);
-		printf("The time stamp is: %d\n", time);
+    encode(data, header, time, result);
 
-		if(!WriteFile(serialHandle, test, 8, &bytesWritten, NULL))
-			printf("error in writing\n");
-	} while(newLoop());
+    #define DEBUG 0
+    if(DEBUG)
+    {
+        printf("The COM port is:       %s\n", comPort);
+        printf("The header is:         %u\n", header);
+        printf("The encoded number is: %.4lf\n", data);
+        printf("The time stamp is:     %d\n", time);
 
-	return 0;
+        printf("\nencoded data is: [");
+        for(int i = 0; i < RRC_DATAPACK_SIZE; i++)
+        {
+            printf("%x%s", result[i], i < RRC_DATAPACK_SIZE - 1? ", " : "");
+        }
+        printf("]\n");
+    }
+
+
+    if(!WriteFile(serialHandle, result, RRC_DATAPACK_SIZE, &bytesWritten, NULL))
+    {
+        printf("error in writing\n");
+        exit(-1);
+    }
+
+    CloseHandle(serialHandle);
+
+    return 0;
 }
